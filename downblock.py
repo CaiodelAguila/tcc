7f72a3fb4a1fb7f93e91e3677f3c05c0c9af2528
Caio Mota del Aguila <caio_aguila@id.uff.br>
2026-06-15 15:44:25 -0300

    Adicionado Residual

--- downblock.py
+++ downblock.py
@@ -1,8 +1,27 @@
 import torch as t
 import torch.nn as nn
 
-def down_block(in_channels, out_channels, kernel_size=4, stride=2, padding=1):
-    layers = [nn.Conv1d(in_channels, out_channels, kernel_size, stride, padding)]
-    layers.append(nn.BatchNorm1d(out_channels))
-    layers.append(nn.ReLU())
-    return nn.Sequential(*layers)
+class downblock(nn.Module):
+    def __init__(self, in_channels, out_channels,kernel_size=4,stride=2,padding=1):
+        super().__init__()
+        
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size, stride, padding)
+        self.bn = nn.BatchNorm1d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        if in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv1d(in_channels, out_channels, kernel_size=1, stride=1, padding=0),
+                nn.BatchNorm1d(out_channels)
+            )
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn(out)
+
+        res = self.shortcut(x)
+        out += res
+
+        out = self.relu(out)
+        return out
--- main.py
+++ main.py
@@ -5,4 +5,17 @@ import audio_cnn as ac
 
 
 def main():
-    return 0
+    num_classes = 10
+    model = ac.AudioNet1D(num_classes=num_classes)
+
+
+    device = t.device("cuda" if t.cuda.is_available() else "cpu")
+    model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = t.optim.Adam(model.parameters(), lr=0.001)
+
+    parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"Number of trainable parameters: {parameters}")
+
+    
