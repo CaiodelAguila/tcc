d9f3ff16ae44eb14cbb7fe541522d266019a0006
Caio <caioaguila@gmail.com>
2026-06-29 16:59:40 -0300

    Basic Fixes

--- main.py
+++ main.py
@@ -60,140 +60,149 @@ class SpeechCommandsDatasetV2(Dataset):
 
 
         return tensor_audio, tensor_label
+if __name__ == '__main__':
+    #Construcao dos caminhos dos arquivos e criacao das variaveis primordiais do modelo
+    caminho_dataset = r"E:\TCC\dataset\SpeechCommands\speech_commands_v0.02"
 
-caminho_dataset = r"E:\TCC\dataset\SpeechCommands\speech_commands_v0.02"
+    caminho_validation = os.path.join(caminho_dataset, "validation_list.txt")
+    caminho_testing = os.path.join(caminho_dataset, "testing_list.txt")
 
-caminho_validation = os.path.join(caminho_dataset, "validation_list.txt")
-caminho_testing = os.path.join(caminho_dataset, "testing_list.txt")
+    with open(caminho_validation, 'r', encoding='utf-8') as f:
+        caminhos_validation = set(line.strip() for line in f.readlines())
+    with open(caminho_testing, 'r', encoding='utf-8') as f:
+        caminhos_testing = set(line.strip() for line in f.readlines())
 
-with open(caminho_validation, 'r', encoding='utf-8') as f:
-    caminhos_validation = set(line.strip() for line in f.readlines())
-with open(caminho_testing, 'r', encoding='utf-8') as f:
-    caminhos_testing = set(line.strip() for line in f.readlines())
+    classes_alvo = [pasta for pasta in os.listdir(caminho_dataset) if os.path.isdir(os.path.join(caminho_dataset, pasta)) and pasta != "_background_noise_"]
 
-classes_alvo = [pasta for pasta in os.listdir(caminho_dataset) 
-                if os.path.isdir(os.path.join(caminho_dataset, pasta)) 
-                and pasta != "_background_noise_"]
+    caminhos_treino,labels_treino = [],[]
+    caminho_validation,labels_validation = [],[]
+    caminho_testing,labels_testing = [],[]
 
-caminhos_treino,labels_treino = [],[]
-caminho_validation,labels_validation = [],[]
-caminho_testing,labels_testing = [],[]
+    for classe in classes_alvo:
+        caminho_classe = os.path.join(caminho_dataset, classe)
+        for arquivo in os.listdir(caminho_classe):
+            if(arquivo.endswith(".wav")):
+                caminho_relativo = f"{classe}/{arquivo}"
+                if caminho_relativo in caminhos_validation:
+                    caminho_validation.append(os.path.join(caminho_classe, arquivo))
+                    labels_validation.append(classe)
+                elif caminho_relativo in caminhos_testing:
+                    caminho_testing.append(os.path.join(caminho_classe, arquivo))
+                    labels_testing.append(classe)
+                else:
+                    caminhos_treino.append(os.path.join(caminho_classe, arquivo))
+                    labels_treino.append(classe)
 
-for classe in classes_alvo:
-    caminho_classe = os.path.join(caminho_dataset, classe)
-    for arquivo in os.listdir(caminho_classe):
-        if(arquivo.endswith(".wav")):
-            caminho_relativo = f"{classe}/{arquivo}"
-            if caminho_relativo in caminhos_validation:
-                caminho_validation.append(os.path.join(caminho_classe, arquivo))
-                labels_validation.append(classe)
-            elif caminho_relativo in caminhos_testing:
-                caminho_testing.append(os.path.join(caminho_classe, arquivo))
-                labels_testing.append(classe)
-            else:
-                caminhos_treino.append(os.path.join(caminho_classe, arquivo))
-                labels_treino.append(classe)
+    print("-"*40)
+    print("Número de amostras no conjunto de treino:", len(caminhos_treino))
+    print("Número de amostras no conjunto de validação:", len(caminho_validation))
+    print("Número de amostras no conjunto de teste:", len(caminho_testing))
+    print("-"*40)
 
-print("-"*40)
-print("Número de amostras no conjunto de treino:", len(caminhos_treino))
-print("Número de amostras no conjunto de validação:", len(caminho_validation))
-print("Número de amostras no conjunto de teste:", len(caminho_testing))
-print("-"*40)
+    classes_unicas = sorted(list(set(labels_treino)))
 
-classes_unicas = sorted(list(set(labels_treino)))
+    label_to_idx = {label: idx for idx, label in enumerate(classes_unicas)}
 
-label_to_idx = {label: idx for idx, label in enumerate(classes_unicas)}
-#Escolha do dataset para treino e teste, utilizando a versão 2 do dataset, que aplica a transformada de Morlet e compressão
-#dataset_treino = SpeechCommandsDataset(caminhos_treino, labels_treino, label_to_idx) #V1
-dataset_treino = SpeechCommandsDatasetV2(caminhos_treino, labels_treino, label_to_idx) #V2
-dataloader_treino = DataLoader(dataset_treino, batch_size=128, shuffle=True,num_workers=4)
+    #Escolha do dataset para treino e teste, utilizando a versão 1 ou 2 do dataset, que aplica a transformada de Morlet e 
+    # compressão dos dados em 256. Threads usadas pro tratamentos dos dados por padrao escolhido em 6.
+    #dataset_treino = SpeechCommandsDataset(caminhos_treino, labels_treino, label_to_idx) #V1
+    dataset_treino = SpeechCommandsDatasetV2(caminhos_treino, labels_treino, label_to_idx) #V2
+    dataloader_treino = DataLoader(dataset_treino, batch_size=128, shuffle=True,num_workers=6)
 
-#dataset_teste = SpeechCommandsDataset(caminho_testing, labels_testing, label_to_idx) #V1
-dataset_teste = SpeechCommandsDatasetV2(caminho_testing, labels_testing, label_to_idx) #V2
-dataloader_teste = DataLoader(dataset_teste, batch_size=128, shuffle=False,num_workers=4)
+    #dataset_teste = SpeechCommandsDataset(caminho_testing, labels_testing, label_to_idx) #V1
+    dataset_teste = SpeechCommandsDatasetV2(caminho_testing, labels_testing, label_to_idx) #V2
+    dataloader_teste = DataLoader(dataset_teste, batch_size=128, shuffle=False,num_workers=6)
+    #Selecionar a placa de video para rodar o treinamento de forma eficiente
+    device = t.device("cuda" if t.cuda.is_available() else "cpu")
+    #Escolha da audionet para o modelo
+    #modelo = ac.AudioNet1D(num_classes=35)  #V1
+    modelo = ac.AudioNet1DV2(num_classes=35) #V2
 
-device = t.device("cuda" if t.cuda.is_available() else "cpu")
-#Escolha da audionet para o modelo
-#modelo = ac.AudioNet1D(num_classes=35)  #V1
-modelo = ac.AudioNet1DV2(num_classes=35) #V2
-modelo = modelo.to(device)
-criterion = nn.CrossEntropyLoss()
-#optimizer = t.optim.Adam(modelo.parameters(), lr=0.0001, weight_decay=0.0001)
-optimizer = t.optim.Adam(modelo.parameters(), lr=0.001)
-
-#checkpoint_path = "checkpoint_audionet.pth" #V1
-checkpoint_path = "checkpoint_audionetv2.pth" #V2
-start=0
-if(os.path.exists(checkpoint_path)):
-    checkpoint = t.load(checkpoint_path)
-    modelo.load_state_dict(checkpoint['model_state'])
-    optimizer.load_state_dict(checkpoint['optim_state'])
-    start = checkpoint['epoch']
-
-num_age = 2
-erro_treino = []
-acc_teste = []
-
-start_time = time.perf_counter()
-for epoch in range(start,num_age):
-    modelo.train()
-    running_loss = 0.0
-    print(f"Epoca : {epoch+1}")
-
-    for lote_idx, (audios, labels) in enumerate(tqdm(dataloader_treino,desc="Training")):
-        audios = audios.to(device)
-        labels = labels.to(device)
-        optimizer.zero_grad()
-        #outputs = modelo(audios.unsqueeze(1)) #V1
-        outputs = modelo(audios) #V2
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-    erro_medio = running_loss / len(dataloader_treino)
-    erro_treino.append(erro_medio)
+    modelo = modelo.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = t.optim.Adam(modelo.parameters(), lr=0.0001, weight_decay=0.0001)
+    #optimizer = t.optim.Adam(modelo.parameters(), lr=0.001)
+    #Carregamento do modelo a partir de um ponto pre treinado por motivos de teste ou aprimoramento
+    #checkpoint_path = "checkpoint_audionet.pth" #V1
+    checkpoint_path = "checkpoint_audionetv2.pth" #V2
+    if(os.path.exists(checkpoint_path)):
+        checkpoint = t.load(checkpoint_path)
+        modelo.load_state_dict(checkpoint['model_state'])
+        optimizer.load_state_dict(checkpoint['optim_state'])
+        start = checkpoint['epoch']
+        erro_treino = checkpoint.get('erro_treino',[])
+        acc_teste = checkpoint.get('acc_teste',[])
+    else:
+        start = 0
+        erro_treino = []
+        acc_teste = []
     
-    modelo.eval()
-    acertos = 0
-    total = 0
-    with t.no_grad():
-        for audios, labels in tqdm(dataloader_teste):
+
+    num_age = 10
+
+    start_time = time.perf_counter()
+    #LOOP de treinamento do modelo gerenciado pelo num_age que define quantas vezes o modelo sera treinado pelo dataset de treino
+    #e para cada iteracao do treino faz um teste sobre o modelo para verificar a constancia do aprendizado e possiveis erros
+    for epoch in range(start,num_age):
+        modelo.train()
+        running_loss = 0.0
+        print(f"Epoca : {epoch+1}")
+
+        for lote_idx, (audios, labels) in enumerate(tqdm(dataloader_treino,desc="Training")):
             audios = audios.to(device)
             labels = labels.to(device)
-
+            optimizer.zero_grad()
             #outputs = modelo(audios.unsqueeze(1)) #V1
             outputs = modelo(audios) #V2
-            _, predicted = t.max(outputs.data, 1)
-            total += labels.size(0)
-            acertos += (predicted == labels).sum().item()
-    acuracia = 100 * acertos / total
-    acc_teste.append(acuracia)
-    checkpoint ={'epoch' : epoch,'model_state' : modelo.state_dict(),'optim_state' : optimizer.state_dict()}
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        erro_medio = running_loss / len(dataloader_treino)
+        erro_treino.append(erro_medio)
     
-    #t.save(checkpoint,"checkpoint_audionet.pth") #V1
-    t.save(checkpoint,"checkpoint_audionetv2.pth") #V2
-    print("[CHECKPOINT]")
-end_time=time.perf_counter()
+        modelo.eval()
+        acertos = 0
+        total = 0
+        with t.no_grad():
+            for audios, labels in tqdm(dataloader_teste,desc="Testing"):
+                audios = audios.to(device)
+                labels = labels.to(device)
 
-print(f"Tempo de treinamento: {end_time-start_time:.4f}")
+                #outputs = modelo(audios.unsqueeze(1)) #V1
+                outputs = modelo(audios) #V2
+                _, predicted = t.max(outputs.data, 1)
+                total += labels.size(0)
+                acertos += (predicted == labels).sum().item()
+        acuracia = 100 * acertos / total
+        acc_teste.append(acuracia)
+        checkpoint ={'epoch' : epoch,'model_state' : modelo.state_dict(),'optim_state' : optimizer.state_dict(),
+                     'erro_treino' : erro_treino,'acc_teste' : acc_teste}
+    
+        #t.save(checkpoint,"checkpoint_audionet.pth") #V1
+        t.save(checkpoint,"checkpoint_audionetv2.pth") #V2
+        print("[CHECKPOINT]")
+    end_time=time.perf_counter()
 
-plt.figure(figsize=(12, 5))
+    print(f"Tempo de treinamento: {end_time-start_time:.4f}")
+    #Plot do grafico de erro medio do modelo e de acuracia pelo tempo de epocas treinadas
+    plt.figure(figsize=(12, 5))
 
-plt.subplot(1, 2, 1)
-plt.plot(range(1, num_age + 1), erro_treino, marker='o', color='red', label='Loss de Treino')
-plt.title('Evolução do Erro (Loss)')
-plt.xlabel('Épocas')
-plt.ylabel('Loss (CrossEntropy)')
-plt.grid(True)
-plt.legend()
+    plt.subplot(1, 2, 1)
+    plt.plot(range(1, len(erro_treino) + 1), erro_treino, marker='o', color='red', label='Loss de Treino')
+    plt.title('Evolução do Erro (Loss)')
+    plt.xlabel('Épocas')
+    plt.ylabel('Loss (CrossEntropy)')
+    plt.grid(True)
+    plt.legend()
 
-plt.subplot(1, 2, 2)
-plt.plot(range(1, num_age + 1), acc_teste, marker='o', color='blue', label='Acurácia de Teste')
-plt.title('Evolução da Acurácia')
-plt.xlabel('Épocas')
-plt.ylabel('Acurácia (%)')
-plt.grid(True)
-plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(range(1, len(acc_teste) + 1), acc_teste, marker='o', color='blue', label='Acurácia de Teste')
+    plt.title('Evolução da Acurácia')
+    plt.xlabel('Épocas')
+    plt.ylabel('Acurácia (%)')
+    plt.grid(True)
+    plt.legend()
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
